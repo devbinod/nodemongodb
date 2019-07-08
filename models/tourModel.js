b62f8b1728db3f86aa77ba5f8054b094d98e1f4c
Binod Pant <bpant@deerhold.org>
2019-07-08 20:19:17 +0545

    fixing bug

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,28 @@ let tourSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    durations: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+
+    ratingAverage: {
+        type: Number,
+        default: 4.5
+    },
+    ratingQuantity: {
+        type: Number,
+        default : 0
+    },
+
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
     rating: {
         required: true,
         type: Number
@@ -14,7 +36,27 @@ let tourSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: true
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim : true
+    },
+    description: {
+        type: String,
+        trim: true,
+        required : [true, 'A tour must have a description']
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDate: [Date]
 });
 
 let Tour = mongoose.model('Tour', tourSchema);
